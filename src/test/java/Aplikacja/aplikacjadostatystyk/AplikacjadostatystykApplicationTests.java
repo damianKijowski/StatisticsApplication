df66d55b1fc79db0263adbfd83396df3ba71e52f
Damian.Kijowski <damian.kijowski01@gmail.com>
2024-09-09 21:10:28 +0200

    initialized postgres database

---
+++ src/test/java/Aplikacja/aplikacjadostatystyk/AplikacjadostatystykApplicationTests.java
@@ -0,0 +1,13 @@
+package Aplikacja.aplikacjadostatystyk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikacjadostatystykApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
